0973997223c03b6c1288789847a194c97e2e0fc3
KyungSik9870 <rudtlr9870@gmail.com>
2022-06-24 00:25:23 +0900

    코틀린에서 변수 다루기

--- src/main/kotlin/Lec01Main.kt
+++ src/main/kotlin/Lec01Main.kt
@@ -5,11 +5,14 @@ fun main() {
     // 불변
     val number2 = 10L
 
+    // 타입을 명시적으로 작성하지 않아도, 타입이 추론된다.
+
     // 초기값이 없으면 타입 지정해줘야함
     var number3: Int
     val number4: Long
+    // Long, long -> 코틀린은 연산이 필요한 경우, 알아서 primitive type 으로 변환한다.
 
-    var number5 = 10L
-
-
+    // nullable 하게 하려면 type 을 지정하고, ? 키워드를 붙여준다.
+    var number5: Long? = 1000L
+    number5 = null;
 }
