2a5c8b25b11fba0f3bc409c994db13bcef211fbb
KyungSik9870 <rudtlr9870@gmail.com>
2022-06-24 00:47:01 +0900

    코틀린에서 null 다루기(3) - 플랫폼 타입

--- src/main/kotlin/Lec02Main.kt
+++ src/main/kotlin/Lec02Main.kt
@@ -8,6 +8,11 @@ fun main() {
     println(str?.length)
     // elvis 연산자 : 앞의 연산 결과가 null 이면 뒤의 값을 사용
     println(str?.length ?: 0)
+
+    val person = Person("name")
+    // nullable 인지 아닌지 모른다면 그것을 '플랫폼 타입' 이라 한다.
+    // 이 떄는 런타임시 NPE 가 발생할 수 있다.
+    println(startsWithA(person.name))
 }
 
 fun startsWithA1(str: String?): Boolean {
@@ -21,4 +26,13 @@ fun startsWithA2(str: String?): Boolean? {
 
 fun startsWithA3(str: String?): Boolean {
     return str?.startsWith("A") ?: false
+}
+
+fun startsWith(str: String?): Boolean {
+    // 이 변수는 Null 이 아니다. NPE 발생 가능
+    return str!!.startsWith("A")
+}
+
+fun startsWithA(str: String): Boolean {
+    return str.startsWith("A")
 }
