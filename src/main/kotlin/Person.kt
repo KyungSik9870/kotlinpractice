f3a73389a122255daac702ef06365c5c7a1fca3c
KyungSik9870 <rudtlr9870@gmail.com>
2022-06-29 00:24:05 +0900

    코틀린에서 클래스를 다루는 방법 - 프로퍼티 접근방법

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -4,5 +4,12 @@ class Person (val name: String = "default", var age: Int = 1) {// 주생성자, 
             throw IllegalArgumentException("$age age is invalid")
         }
     }
+
+    // 함수로 만드는 것도 가능. 프로퍼티처럼 만들 수 있다
+    val isAdult: Boolean
+        get() = this.age >= 20
+
+    val uppercaseName: String
+        get() = this.name.toUpperCase()
 }
 
