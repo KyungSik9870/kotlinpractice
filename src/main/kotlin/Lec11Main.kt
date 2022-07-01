fa29325617849c07c0efad97334826786d8cea8c
KyungSik9870 <rudtlr9870@gmail.com>
2022-07-01 10:47:35 +0900

    코틀린에서 접근제어자를 다루는 방법

---
+++ src/main/kotlin/Lec11Main.kt
@@ -0,0 +1,15 @@
+/**
+ * 코틀린에서 접근 제어를 다루는 방법
+ */
+fun main() {
+
+    // java - protected : 같은 패키지 또는 하위 클래스에서만 접근 가능
+    // kotlin - protected : 선언된 클래스 또는 하위 클래스에서만 접근 가능
+
+    // java - default : 같은 패키지에서만 접근 가능
+    // kotlin - internal : 같은 모듈에서만 접근 가능
+
+    // kotlin 에서느 public 이 default 임
+
+
+}
