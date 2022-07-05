abc124c3b66f2df2a67e912f5887df0c8d703952
KyungSik9870 <rudtlr9870@gmail.com>
2022-07-05 23:56:22 +0900

    코틀린에서 배열, 컬렉션을 다루는 방법

---
+++ src/main/kotlin/Lec15Main.kt
@@ -0,0 +1,11 @@
+/**
+ * 코클린에서 배열, 컬렉션을 다루는 방법
+ */
+fun main() {
+    val numbers = listOf(100, 200) // 불변 리스트
+    val numbers2 = mutableListOf(100, 200) // 가변 리스트
+
+    val map = mapOf<Int, String>()
+
+    // 그 외에 nullable 한 것과 그렇지 않은 것에 대한 주의가 필요.
+}
